13259542323b657ef90e0d68daca57e00406f7c6
Lucario387 <hoangtun0810@gmail.com>
2023-02-18 13:33:57 +0900

    V2.0 full lazy update (#42)
    * Add types
    
    * Update Types

--- nvchad_types/chadrc.lua
+++ nvchad_types/chadrc.lua
@@ -1,66 +1,186 @@
 ---@meta
 
 ---@class ChadrcConfig
----@field ui? UIConfig
----@field plugins? NvPluginsTable If a `PluginName` is set to `false`, remove it from the plugin config
----@field mappings? MappingsConfig
+local chadrc = {
+  --- UI related configuration
+  --- e.g. colorschemes, statusline themes, cmp themes, dashboard, some LSP ui related
+  --- @class UIConfig
+  ui = {
+    --- List of highlights group to add.
+    --- Should be highlights that is not a part of base46 defeault integration
+    --- @see https://github.com/NvChad/base46/tree/master/lua/base46/integrations
+    --- @type HLTable
+    hl_add = nil,
+    --- List of highlight groups that is part of base46 default integration that you want to change
+    --- @see https://github.com/NvChad/base46/tree/master/lua/base46/integrations
+    --- @type HLTable
+    hl_override = nil,
+    --- @see https://github.com/NvChad/base46/tree/master/lua/base46/themes for the colors of each theme
+    --- @type {[ThemeName]: Base46Table}
+    changed_themes = {},
+    --- A table with 2 strings, denoting the themes to toggle between.
+    --- One of the 2 strings must be the value of `theme` field
+    --- @type {[1]: string, [2]: string}
+    theme_toggle = { "onedark", "one_light" },
+    --- Enable transparency or not
+    --- @type boolean
+    transparency = nil,
+    --- Theme to use.
+    --- You can try out the theme by executing `:Telescope themes`
+    --- @see https://github.com/NvChad/base46/tree/master/lua/base46/themes
+    --- @type string
+    theme = nil,
 
----@class UIConfig
----@field hl_add? HLTable List of highlight groups to add. Should be highlights that is not a part of base46 definition. Check [base46 integrations](https://github.com/NvChad/base46/tree/master/lua/base46/integrations) for more information
----@field hl_override? HLTable List of highlight groups that will override the highlight inside each integration. Check [base46 integrations](https://github.com/NvChad/base46/tree/master/lua/base46/integrations) for more information
----@field theme_toggle? {[1]: string, [2]: string} the theme you want to toggle between. One of them has to be the same with the one set in `theme`
----@field transparency? boolean Whether to compiled with transparent background or not
----@field theme? string theme to use. List of theme can be find on [base46 Repo](https://github.com/NvChad/base46/tree/master/lua/base46/themes)
----@field cmp? NvCmpConfig options for UI of nvim-cmp popup menu
----@field statusline? NvStatusLineConfig options for NvChad custom statusline
----@field tabufline? NvTabufLineConfig options for NvChad custom tabline
----@field nvdash? NvDashConfig Options for NvChad custom dashboard menu
----@field cheatsheet? NvCheatSheetConfig Options for NvChad cheatsheet menu
----@field changed_themes {[ThemeName]: Base46Table} For users who want to override default colors used in a colorscheme. Check out [Theme directory](https://github.com/NvChad/base46/tree/master/lua/base46/themes) for the list of theme name that will be registered by NvChad
+    --- @class NvCmpConfig
+    cmp = {
+			--- Whether to add colors to icons in nvim-cmp popup menu
+			--- @type boolean
+			icons = nil,
+			--- Whether to also have the lsp kind highlighted with the icons as well or not
+			--- @type boolean
+			lspkind_text = nil,
+			--- nvim-cmp style
+			--- @type '"default"'|'"flat_light"'|'"flat_dark"'|'"atom"'|'"atom_colored"'
+			style = nil,
+			--- Only has effects when the style is `default`
+			--- @type string|Base30Colors
+			border_color = nil,
+      --- Whether to have more vibrant color for the currently selected entry in the popup menu
+      --- @type "colored"|"simple"
+      selected_item_bg = nil,
+    },
+    --- @class NvStatuslineConfig
+    statusline = {
+			--- statusline theme
+			--- @type '"default"'|'"vscode"'|'"vscode_colored"'|'"minimal"'
+			theme = nil,
+			--- Separator style for NvChad Statusline
+      ---     - Only when the *theme* is `minimal`, "round" or "block" will be having effect
+			--- @type '"default"'|'"round"'|'"block"'
+			separator_style = nil,
 
----@class NvCmpConfig
----@field icons boolean Whether to add colors to icons in nvim-cmp popup menu
----@field lspkind_text boolean Whether to also have the lsp kind highlighted with the icons as well or not
----@field style '"default"'|'"flat_light"'|'"flat_dark"'|'"atom"'|'"atom_colored"' nvim-cmp style
----@field border_color string|Base30Colors Only has effects when the style is `default`
----@field selected_item_bg '"colored"'|'"simple"'
+      --- List of modules that you overirde
+      --- Check https://github.com/NvChad/ui/blob/main/lua/nvchad_ui/statusline/modules.lua for the list of modules
+      --- @type fun(): table
+      overriden_modules = nil,
+    },
+    --- @class NvTablineConfig
+    tabufline = {
+      --- Whether to use/load tabufline or not
+      --- @type boolean
+      enabled = nil,
+      --- If false, load tabufline on startup
+      --- If true, load tabufline when there is at least 2 buffers opened
+      --- @type boolean
+      lazyload = nil,
+      --- List of modules that you overirde
+      --- Check https://github.com/NvChad/ui/blob/main/lua/nvchad_ui/tabufline/modules.lua for the list of modules
+      --- @type fun(): table
+      overriden_modules = nil,
+    },
+    --- @class NvDashboardConfig
+    nvdash = {
+      --- Whether to open dashboard on opening nvim 
+      --- @type boolean
+      load_on_startup = false,
+      --- Your ascii art
+      --- Each string is one line
+      --- @type string[],
+      header = nil,
+      --- List of buttons to show on the dashboard
+      --- @type NvDashButtonConfig[]
+      buttons = nil,
+    },
+    --- @class NvCheatsheetConfig
+    cheatsheet = {
+      --- Cheatsheet theme
+      --- @type '"grid"'|'"simple"'
+      theme = nil,
+    },
+  },
+  
+  --- A table of mappings
+  ---     - `disabled` is used to define the keymaps that you don't want to keep
+  ---     - Other keys are the list of default tables that is with NvChad
+  ---     - You can define your custom table, such as the example below
+  --- ```lua
+  --- M.mappings = {
+  ---   ["some table name"] = {
+  ---     -- plugin = true, -- will make this table load only when you specify it to
+  ---     ["some vim mode"] = {
+  ---       ["some lhs"] = {
+  ---         "rhs of a keymap", -- this must be here. This can also be a Lua function
+  ---         "Description for the keymap",
+  ---         opts = {}, -- Other opts for the keymaps
+  ---       }
+  ---     }
+  ---   }
+  --- }
+  --- ```
+  --- @see core.mappings
+  --- @type MappingsTable
+  mappings = nil,
 
----@class NvStatusLineConfig
----@field theme '"default"'|'"vscode"'|'"vscode_colored"'|'"minimal"' statusline theme
----@field separator_style '"default"'|'"round"'|'"block"' As of now, `round` and `block` only works for minimal theme
----@field overriden_modules? fun(): table List of modules that you override. Check [UI Plugins](https://nvchad.com/config/nvchad_ui) for more information
+  --- The module to be imported and merged with the default plugin settings
+  --- @type string
+  plugins = "",
 
----@class NvTabufLineConfig
----@field enabled boolean whether to enable custom tabufline or not
----@field lazyload boolean If true, will only load custom tabufline if there's two or more buffers/tabs
----@field overriden_modules? fun(): table List of modules that you override. Check [UI Plugins](https://nvchad.com/config/nvchad_ui) for more information
+  --- Lazy.nvim setup opts
+  --- Check `:h lazy.nvim-configuration` for the exact options
+  --- @type table
+  lazy_nvim = {},
+}
 
----@class NvDashConfig
----@field load_on_startup boolean If true, load in dashboard when opening nvim
----@field header string[] Your ascii art
----@field buttons NvDashButtonConfig[] List of buttons on the dashboard
 
 ---@class NvDashButtonConfig
 ---@field [1] string Description for the button
 ---@field [2] string sequence of keys to press to trigger the keybind
 ---@field [3] string|fun() A Vim Command/A Lua function to be triggered when pressing the keybind/pressing enter on the line with the description on the dashboard
 
----@class NvCheatSheetConfig
----@field theme '"grid"'|'"simple"' Cheat sheet theme
+---@alias MappingsTable DefaultMappingsTable | table<string, KeymapsTable>
 
+--- @class DefaultMappingsTable
+--- @field disabled   DisabledTable Keymaps to be removed
+--- @field general    KeymapsTable Keymaps that will be load on startup
+--- @field tabufline  KeymapsTable Keymaps that will be load with NvChad's tabline
+--- @field comment    KeymapsTable Keymaps for Comment.nvim
+--- @field lspconfig  KeymapsTable Keymaps for nvim-lspconfig
+--- @field nvimtree   KeymapsTable Keymaps for nvim-tree.lua
+--- @field telescope  KeymapsTable Keymaps for telescope.nvim
+--- @field nvterm     KeymapsTable Keymaps for NvChad/nvterm
+--- @field whichkey   KeymapsTable Keymaps for which-key.nvim
+--- @field blankline  KeymapsTable Keymaps for indent-blankline.nvim
+--- @field gitsigns   KeymapsTable Keymaps for gitsigns.nvim
 
 --- List of keymaps that is part of `core/mappings.lua` that will be removed
----@alias DisabledTable {[VimKeymapMode]: DisabledKeymaps}
+---@class DisabledTable 
+---@field n   table<string, '""'|false> Normal Mode keymaps to remove
+---@field x   table<string, '""'|false> Visual Mode keymaps to remove
+---@field s   table<string, '""'|false> Select Mode keymaps to remove
+---@field v   table<string, '""'|false> Visual + Select Mode keymaps to remove
+---@field o   table<string, '""'|false> Operator-Pending Mode keymaps to remove
+---@field i   table<string, '""'|false> Insert Mode keymaps to remove
+---@field c   table<string, '""'|false> Command-Line Mode keymaps to remove
+---@field l   table<string, '""'|false> Insert + Command-Line + Lang-Arg Mode keymaps to remove
+---@field t   table<string, '""'|false> Terminal Mode keymaps to remove
+---@field "!" table<string, '""'|false> Insert + Command-Line Mode keymaps to remove
+---@field ""  table<string, '""'|false> Normal, Visual and Operating-Pending Mode keymaps to remove
 
----@alias DisabledKeymaps {[string]: '""'|false}
-
----@alias MappingsConfig {disabled: DisabledTable, [string]: MappingsTable}
-
----@alias MappingsTable {[VimKeymapMode]: table<lhs, KeymapConfig>}
-
----@alias lhs string
+---@class KeymapsTable
+---@field plugin boolean Whether this whole table will be loaded on startup or not
+---@field n   table<string, KeymapConfig> Normal Mode keymaps
+---@field x   table<string, KeymapConfig> Visual Mode keymaps
+---@field s   table<string, KeymapConfig> Select Mode keymaps
+---@field v   table<string, KeymapConfig> Visual + Select Mode keymaps
+---@field o   table<string, KeymapConfig> Operator-Pending Mode keymaps
+---@field i   table<string, KeymapConfig> Insert Mode keymaps
+---@field c   table<string, KeymapConfig> Command-Line Mode keymaps
+---@field l   table<string, KeymapConfig> Insert + Command-Line + Lang-Arg Mode keymaps
+---@field t   table<string, KeymapConfig> Terminal Mode keymaps
+---@field "!" table<string, KeymapConfig> Insert + Command-Line Mode keymaps
+---@field ""  table<string, KeymapConfig> Normal, Visual and Operating-Pending Mode keymaps
 
 ---@class KeymapConfig
 ---@field [1] string|fun() A Vimscript string or a Lua function. `rhs` of the keymap
 ---@field [2] string Description for the keymap
----@field [3] NvKeymapOpts? List of additional options for the keymap
+---@field opts NvKeymapOpts? List of additional options for the keymap
--- nvchad_types/plugins.lua
+++ nvchad_types/plugins.lua
@@ -2,47 +2,117 @@
 
 ---@alias PluginName string
 
----Adopted from lazy.nvim README.md
----Check `:h lazy.nvim` for more information
----@class PluginConfig
----@field dir? string A directory pointing to a local plugin
----@field url? string A custom git url where the plugin is hosted
----@field name? string A custom name for the plugin used for the local plugin directory, and as the display name of the plugin
----@field dev? boolean If true, A local plugin directory will be used
----@field lazy? boolean If true, the plugin will only be loaded when necessary
----@field enabled? boolean|fun():boolean Specify if the plugin should be included in the spec
----@field cond? boolean|fun():boolean Specify condition for whether the plugin should be loaded (Useful for specifying plugins for VsCode/FireNvim) 
----@field dependencies? string|LazyDependencyConfig[] List of plugin names or plugin specs that should be loaded when the plugin loads. If you only specify the name, make sure the plugin config is specified somewhere else
----@field opts? table|fun(opts: table):table Should be a table/returns a table of configuration option for a plugin. If this is set, `config` argument will be considered as `true`
----@field config? function|true If true, assume and execute `require(plugin_name).setup(opts)`, with opts that can be modified using the `opts` argument of the plugin config. If a function, then this will be executed appropriately
----@field init? fun(self: PluginConfig) The function to be run during startup. This will run regardless of the plugin being loaded or not
----@field build? string Executed when a plugin is installed or updated. If it's a string, it will be ran as a shell command. When prefixed with `:` it will be considered a Neovim command.
----@field branch? string Specify branch of the Git repo to install/track
----@field tag? string Specify tag of the Git repo to install/track
----@field commit? string Specify commit id for installation
----@field version? string A specific version to use from. Supports full [Semver](https://devhints.io/semver) ranges
----@field pin? boolean If true, this plugin will not be updated
----@field event? string|string[] This plugin will be loaded when the Vim Event(s) happens. For more information, see `:h events`, `:h diagnostic-events`, `:h lsp-events`
----@field cmd? string|string[] The plugin will be load when the Vim command(s) is triggered
----@field ft? string|string[] When opening/setting a file with filetype(s) in this specified list, the plugin will be loaded
----@field keys? string|string[]|LazyKeymaps[]|fun(self:PluginConfig, keys:string[]):(string|LazyKeymaps)[] Specify some sequence of keybinds that will load this plugin
----@field module? false If specified, will not automatically load this Lua module when it's required by some other plugin(s)
----@field priority? number Only useful for **start** plugins to force loading order. Higher number means higher priority. Default priority is **50**
 
----@class NvPluginConfig : PluginConfig
----@field override_options? table|fun():table Table format of your settings that will override NvChad's default options. The default settings you don't change here will remain in effect.
----@field rm_default_opts? boolean If true, the default NvChad options will be removed.
+---@class ImportPluginConfig
+local importConfig = {
+  --- Name of the module to be imported.
+  --- If this field is inside a table, all other keys inside such table will not be used
+  --- @type string
+  import = nil,
+}
 
----@class LazyPluginConfig: PluginConfig
----@field [1] string Short plugin url. Equivalent of `url` argument
+--- Adopted from lazy.nvim README.md
+--- Check `:h lazy.nvim` for more information
+--- @class PluginSpec
+local pluginConfig = {
+	--- A directory pointing to a local plugin
+  --- @type string
+	dir = nil,
+	--- A custom git url where the plugin is hosted
+  --- @type string
+	url = nil,
+	--- A custom name for the plugin used for the local plugin directory, and as the display name of the plugin
+  --- @type string
+	name = nil,
+	--- If true, A local plugin directory will be used
+  --- @type string
+	dev = nil,
+	--- If true, the plugin will only be loaded when necessary
+  --- @type boolean
+	lazy = nil,
+	--- Specify if the plugin should be included in the spec
+  --- @type boolean|fun():boolean
+	enabled = nil,
+	--- Specify condition for whether the plugin should be loaded (Useful for specifying plugins for VsCode/FireNvim) 
+  --- @type boolean|fun():boolean
+	cond = nil,
+	--- List of plugin names or plugin specs that should be loaded when the plugin loads. 
+  --- If you only specify the name, it will be installed and loaded with other(s) dependent plugins
+  --- @type PluginConfig|string|(PluginConfig|string)[]
+	dependencies = nil,
+	--- Should be a table/returns a table of configuration option for a plugin. 
+  ---     - If `opts` is a function, then it is a function that pass in the defaults opts table for that plugin from NvChad
+  --- @type table|fun(opts: table): table
+	opts = nil,
+	--- What to do after plugin is loaded
+  ---     - If `true`, then lazy will do `require(plugin_name).setup(opts)`, with `opts` is the table from the `opts` field (or nil if not defined)
+  ---     - If `config` is a function with no arguments, run the function as is
+  ---     - If `config` is a function with 2 arguments, the second argument will be the opts table that is NvChad's default config merged with the `opts` table in your config. The first argument is for internal use, it is not needed for general usage
+  --- @type fun()|fun(self: PluginConfig, opts: table)|true
+	config = nil,
+	--- fun(self: PluginConfig) The function to be run during startup. This will run regardless of the plugin being loaded or not
+	init = nil,
+	--- Executed when a plugin is installed or updated. 
+  ---     - If it's a string, it will be ran as a shell command. 
+  ---     - When prefixed with `:` it will be considered a Neovim command.
+  ---     - If it's a list of string, execute them in sequential. 
+  ---     - If a function, then the function will be executed
+  --- @type fun()|string|string[]
+	build = nil,
+	--- Specify branch of the Git repo to install/track for updates
+  --- @type string
+	branch = nil,
+	--- Specify tag of the Git repo to install
+  --- @type string
+	tag = nil,
+	--- Specify commit id for installation
+  --- @type string
+	commit = nil,
+	--- A specific version to use from. Supports full [Semver](https://devhints.io/semver) ranges
+  --- @type string,
+	version = nil,
+	--- If true, this plugin will not be updated
+  --- @type boolean
+	pin = nil,
+  --- When false, submodules will not be fetched
+  --- Defaults to true
+  --- @type boolean
+  submodules = true,
+	--- Events that will trigger the loading of this plugin
+  ---     - If it's a string/list of strings, the strings can be a simple VimEvent like `BufEnter`, or with patterns like `BufEnter *.vim`
+  ---     - If a function, then the second argument of the function are the list of events that defined the loading of this plugin by NvChad
+  --- Checkout `:h events`, `:h lsp-events`, `:h diagnostics-events` for the list of available events
+  --- @type string|string[]|fun(self: PluginConfig, event: string[]): string[]
+	event = nil,
+	--- (List of) Ex command(s) that will trigger the loading of this plugin
+  --- Also can be a function, with the second argument are the list of Ex command that defined the loading of this plugin by NvChad
+  --- @type string|string[]|fun(self: PluginConfig, cmd: string[]): string[]
+	cmd = nil,
+	--- Files with types defined in this list will trigger the loading of this plugin
+  ---     - If a function, then the second argument of the function are the list of filetypes that defined the loading of this plugin by NvChad
+  --- @type string|string[]|fun(self: PluginConfig, ft:string[]): string[]
+	ft = nil,
+	--- Specify some sequence of keybinds that will load this plugin
+  ---     - If a function, then the second argument of the function are the list of key sequences that defined the loading of this plugin by NvChad
+  --- @type string|string[]|LazyKeymaps[]|fun(self: PluginConfig, keys: string[]): (string|LazyKeymaps)[]
+	keys = nil,
+	--- If specified, will not automatically load this Lua module when it's required by some other plugin(s)
+  --- @type boolean
+	module = nil,
+	--- Only useful for **start** plugins to force loading order. Higher number means higher priority. Default priority is **50**
+  --- @type integer
+	priority = nil,
+}
 
----@alias LazyDependencyConfig string|LazyPluginConfig
 
----@alias NvPluginsTable {[PluginName]: NvPluginConfig|false} # If a `PluginName` is false, remove it from the plugin table (will not be passed to lazy.nvim)
+---@class PluginConfig: PluginSpec
+---@field [1] string If the first field in the table is a string and not a key-value pair, it will be considered as the url for the plugin
+
+---@alias NvPluginConfig PluginConfig | ImportPluginConfig
 
 ---Check `:h vim.keymap.set()` for more information
 ---@class LazyKeymaps
----@field [1] string lhs of the mapping
+---@field [1] string lhs of the mapping. Must always exist
 ---@field [2]? string|fun()|false rhs. Can be a vimscript string or a Lua function
 ---@field desc? string
 ---@field mode? VimKeymapMode|VimKeymapMode[]
